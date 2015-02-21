6d73721fd27c192fb472a6d500552568194166e9
rob <rsreed19@gmail.com>
2015-02-21 14:04:08 -0700

    created a new scene to test the hud.

--- Practice2d/Assets/Scripts/gui/CursorScript.cs
+++ Practice2d/Assets/Scripts/gui/CursorScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class CursorScript : MonoBehaviour {
 
+	public GameObject radialSelection;
+	public Animator cursorAnim;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -13,5 +16,17 @@ public class CursorScript : MonoBehaviour {
 	void Update () 
 	{
 		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			cursorAnim.Play("CursorClickStartAnimation", -1, 0);
+			radialSelection.transform.position = transform.position;
+			cursorAnim.Play("RadialClickAnimation", 1, 0);
+		}
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			cursorAnim.Play("CursorClickEndAnimation", -1, 0);
+		}
 	}
 }
