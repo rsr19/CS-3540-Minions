1d3d6759eedd18b86618d96c0e84d81edfc5d2a7
Matthew Robertson <Matthewlyle321@gmail.com>
2015-03-16 18:02:35 -0600

    added a new death animation, and the minion now dies when the hero jumps on him. added goal flag

---
+++ Practice2d/Assets/destructible.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destructible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Minion")
+						Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
