ba1e9d9414ab8bc8892bdd272a78a7b7398aa885
Matthew Robertson <Matthewlyle321@gmail.com>
2015-03-16 20:51:22 -0600

    working on a new minion he is still incomplete

---
+++ Practice2d/Assets/TowerTest.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class TowerTest : MonoBehaviour {
+	/*
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if the player presses the left mouse button
+		if(Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKeyDown (KeyCode.Space))
+		{
+			//convert the mouse location to world location
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+			
+			//get the direction from the player to the mouse world location
+			Vector2 mouseDir = new Vector2 (mousePos.x - transform.position.x, mousePos.y - transform.position.y).normalized;
+			
+			//instantiate the bullet
+			GameObject obj = (GameObject)Instantiate(bullet, 
+			                                         new Vector2(transform.position.x + bulletDist * mouseDir.x, transform.position.y + bulletDist * mouseDir.y),
+			                                         Quaternion.identity);
+			//set the direction of the bullet
+			obj.GetComponent<BulletScript2>().direction = mouseDir;
+		}
+
+	
+	}
+	*/
+}
