df758677a9e7149894490b53eba77a68194c06af
Matthew Robertson <Matthewlyle321@gmail.com>
2015-04-07 12:02:26 -0600

    made changes to level 4, and added slower and jumper to prefabs.

---
+++ Practice2d/Assets/Slower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slower : MonoBehaviour {
+
+	public float slowerSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Hero") 
+		{
+			collision.GetComponent<HeroAI>().velocity = slowerSpeed;
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
